d0054ccf96fb83c4530d4ab9ea3e2badaea901a3
Dong_Smartosc <hiepthanhts@gmail.com>
2022-05-31 14:32:51 +0700

    update page location

--- pages/locations.vue
+++ pages/locations.vue
@@ -24,36 +24,231 @@
           ><span class="location_name">Hai Phong</span>
         </div>
       </div>
+      <div class="box_btn_reservation">
+        <div class="btn_reservation">
+          <img
+            src="https://pizza4ps.com/common2017/img/other/ico_reser.png"
+            alt=""
+          />
+          <span class="btn_name">Reservation Online</span>
+        </div>
+      </div>
+      <div class="box_btn_phonenum">
+        <div class="btn_phonenum">
+          <img
+            src="https://pizza4ps.com/common2017/img/other/ico_tel2.svg"
+            alt=""
+          />
+          <span class="text_phone">1900 6043</span>
+        </div>
+      </div>
       <div class="intro">
-        <div class="box_btn_reservation">
-          <div class="btn_reservation">
+        <div class="box_location_desciption">
+          <div class="left">
+            <h4>Pizza 4P’s Aeon Mall – HP</h4>
+            <div class="box_description">
+              <div class="box_address">
+                <img
+                  src="https://pizza4ps.com/common2017/img/other/ico_map.svg"
+                  alt=""
+                />
+                <span
+                  >10 Vo Nguyen Giap, Kênh Dương , Lê Chân district, Hải
+                  Phòng</span
+                >
+              </div>
+              <div class="box_time">
+                <img
+                  src="https://pizza4ps.com/common2017/img/other/ico_time.svg"
+                  alt=""
+                />
+                <div class="box_time_detail">
+                  <span>Mon-Fri 10:00-22:00 (Last Order 21:30)</span>
+                  <span>Sat-Sun 09:00-22:00 (Last Order 21:30)</span>
+                </div>
+              </div>
+              <div class="go_to_ggmap">Go to Google Map ></div>
+              <div class="box_btn">
+                <span class="btn_see_detail">See Detail</span>
+                <span class="btn_seservation">Reservation</span>
+              </div>
+            </div>
+          </div>
+          <div class="right">
             <img
-              src="https://pizza4ps.com/common2017/img/other/ico_reser.png"
+              src="https://pizza4ps.com/admin/wp-content/uploads/2020/12/photo_2020-12-09_12-16-58-768x1024.jpg"
               alt=""
             />
-            <span class="btn_name">Reservation Online</span>
+            <iframe
+              class="iframe_map"
+              width="320"
+              height="320"
+              style="border: 0"
+              loading="lazy"
+              allowfullscreen
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d932.2551786049297!2d106.68313880928991!3d20.830869988452342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd5d63b3a67d36ecd!2sPizza%204P's%20Aeon%20Mall%20L%C3%AA%20Ch%C3%A2n!5e0!3m2!1svi!2s!4v1607486425290!5m2!1svi!2s"
+            ></iframe>
           </div>
         </div>
-        <div class="left">
-          <span class="title_page">Location</span><br />
-          <!-- <span>Hai Phong</span><span class="separator">|</span>
-          <span>Ho Chi Minh</span><span class="separator">|</span>
-          <span>Ha Noi</span><span class="separator">|</span>
-          <span>Da Nang</span><span class="separator">|</span>
-          <span>Nha Trang</span><span class="separator">|</span>
-          <span>Phnom Penh</span> -->
+        <div class="box_dear_custommer">
+          <div>
+            <strong>Dear Landlords,</strong><br />
+            <span
+              >Pizza 4P's plans to open new locations throughout Vietnam, as
+              well as in other countries of Southeast Asia.We are always in
+              search of unique properties with the capability to express the
+              world of Pizza 4P's.If you have information on potential
+              properties and locations, please contact us here.
+            </span>
+          </div>
         </div>
-        <div class="right">
-          <p>
-            With our mission of "Delivering Wow, Sharing Happiness," we have
-            been striving every day to provide the best dining experience to all
-            of our dear guests. A restaurant should not be a place to simply
-            have a meal. We truly want every guest to leave happy with our food,
-            service, and environment that delivers the "Wow" factor. We continue
-            to evolve and improve every day so that we can always fulfill our
-            mission of "Delivering Wow, Sharing Happiness" to every guest.
-          </p>
-          <p>About Pizza 4P's</p>
+      </div>
+      <div class="intro">
+        <div class="box_location_desciption">
+          <div class="left">
+            <h4>Pizza 4P’s Aeon Mall – HP</h4>
+            <div class="box_description">
+              <div class="box_address">
+                <img
+                  src="https://pizza4ps.com/common2017/img/other/ico_map.svg"
+                  alt=""
+                />
+                <span
+                  >10 Vo Nguyen Giap, Kênh Dương , Lê Chân district, Hải
+                  Phòng</span
+                >
+              </div>
+              <div class="box_time">
+                <img
+                  src="https://pizza4ps.com/common2017/img/other/ico_time.svg"
+                  alt=""
+                />
+                <div class="box_time_detail">
+                  <span>Mon-Fri 10:00-22:00 (Last Order 21:30)</span>
+                  <span>Sat-Sun 09:00-22:00 (Last Order 21:30)</span>
+                </div>
+              </div>
+              <div class="go_to_ggmap">Go to Google Map ></div>
+              <div class="box_btn">
+                <span class="btn_see_detail">See Detail</span>
+                <span class="btn_seservation">Reservation</span>
+              </div>
+            </div>
+          </div>
+          <div class="right">
+            <img
+              src="https://pizza4ps.com/admin/wp-content/uploads/2020/12/photo_2020-12-09_12-16-58-768x1024.jpg"
+              alt=""
+            />
+            <iframe
+              class="iframe_map"
+              width="320"
+              height="320"
+              style="border: 0"
+              loading="lazy"
+              allowfullscreen
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d932.2551786049297!2d106.68313880928991!3d20.830869988452342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd5d63b3a67d36ecd!2sPizza%204P's%20Aeon%20Mall%20L%C3%AA%20Ch%C3%A2n!5e0!3m2!1svi!2s!4v1607486425290!5m2!1svi!2s"
+            ></iframe>
+          </div>
+        </div>
+        <div class="box_dear_custommer">
+          <div>
+            <strong>Dear Landlords,</strong><br />
+            <span
+              >Pizza 4P's plans to open new locations throughout Vietnam, as
+              well as in other countries of Southeast Asia.We are always in
+              search of unique properties with the capability to express the
+              world of Pizza 4P's.If you have information on potential
+              properties and locations, please contact us here.
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="content-wrap">
+      <div class="header_location">
+        <div class="box_title">
+          <span class="separator">|</span
+          ><span class="location_name">Hai Phong</span>
+        </div>
+      </div>
+      <div class="box_btn_reservation">
+        <div class="btn_reservation">
+          <img
+            src="https://pizza4ps.com/common2017/img/other/ico_reser.png"
+            alt=""
+          />
+          <span class="btn_name">Reservation Online</span>
+        </div>
+      </div>
+      <div class="box_btn_phonenum">
+        <div class="btn_phonenum">
+          <img
+            src="https://pizza4ps.com/common2017/img/other/ico_tel2.svg"
+            alt=""
+          />
+          <span class="text_phone">1900 6043</span>
+        </div>
+      </div>
+      <div class="intro">
+        <div class="box_location_desciption">
+          <div class="left">
+            <h4>Pizza 4P’s Aeon Mall – HP</h4>
+            <div class="box_description">
+              <div class="box_address">
+                <img
+                  src="https://pizza4ps.com/common2017/img/other/ico_map.svg"
+                  alt=""
+                />
+                <span
+                  >10 Vo Nguyen Giap, Kênh Dương , Lê Chân district, Hải
+                  Phòng</span
+                >
+              </div>
+              <div class="box_time">
+                <img
+                  src="https://pizza4ps.com/common2017/img/other/ico_time.svg"
+                  alt=""
+                />
+                <div class="box_time_detail">
+                  <span>Mon-Fri 10:00-22:00 (Last Order 21:30)</span>
+                  <span>Sat-Sun 09:00-22:00 (Last Order 21:30)</span>
+                </div>
+              </div>
+              <div class="go_to_ggmap">Go to Google Map ></div>
+              <div class="box_btn">
+                <span class="btn_see_detail">See Detail</span>
+                <span class="btn_seservation">Reservation</span>
+              </div>
+            </div>
+          </div>
+          <div class="right">
+            <img
+              src="https://pizza4ps.com/admin/wp-content/uploads/2020/12/photo_2020-12-09_12-16-58-768x1024.jpg"
+              alt=""
+            />
+            <iframe
+              class="iframe_map"
+              width="320"
+              height="320"
+              style="border: 0"
+              loading="lazy"
+              allowfullscreen
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d932.2551786049297!2d106.68313880928991!3d20.830869988452342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd5d63b3a67d36ecd!2sPizza%204P's%20Aeon%20Mall%20L%C3%AA%20Ch%C3%A2n!5e0!3m2!1svi!2s!4v1607486425290!5m2!1svi!2s"
+            ></iframe>
+          </div>
+        </div>
+        <div class="box_dear_custommer">
+          <div>
+            <strong>Dear Landlords,</strong><br />
+            <span
+              >Pizza 4P's plans to open new locations throughout Vietnam, as
+              well as in other countries of Southeast Asia.We are always in
+              search of unique properties with the capability to express the
+              world of Pizza 4P's.If you have information on potential
+              properties and locations, please contact us here.
+            </span>
+          </div>
         </div>
       </div>
     </section>
@@ -125,37 +320,212 @@ export default {
       }
     }
   }
+  > .box_btn_reservation {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin: 5em auto;
+    margin-bottom: 2em;
+    > .btn_reservation {
+      cursor: pointer;
+      background: #394f80;
+      color: white;
+      display: flex;
+      justify-content: center;
+      width: 450px;
+      padding: 2em 0;
+      > img {
+        width: 29px;
+        height: 29px;
+        margin-right: 16px;
+        margin-top: 4px;
+      }
+      > .btn_name {
+        font-size: 24px;
+      }
+    }
+    > .btn_reservation:hover {
+      opacity: 0.6;
+    }
+  }
+  > .box_btn_phonenum {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+    margin-bottom: 4em;
+    > .btn_phonenum {
+      border-top: 1px dashed #808080;
+      border-bottom: 1px dashed #808080;
+      padding: 8px 8px;
+      > img {
+        width: 18px;
+        margin-right: 8px;
+      }
+      > .text_phone {
+        font-size: 22px;
+        font-family: 'Times New Roman';
+        letter-spacing: 0.07em;
+      }
+    }
+  }
   > .intro {
     display: flex;
-    padding: 90px 0;
+    padding: 90px 0 0;
     max-width: 60%;
     flex-wrap: wrap;
     margin: 0 auto;
-    > .box_btn_reservation {
+    > .box_location_desciption {
       width: 100%;
       display: flex;
-      justify-content: center;
+      justify-content: space-between;
       margin: 0 auto;
-      > .btn_reservation {
-        cursor: pointer;
-        background: #394f80;
-        color: white;
-        display: flex;
-        justify-content: center;
-        width: 450px;
-        > img {
-          width: 29px;
-          height: 29px;
-          margin-right: 16px;
-          margin-top: 4px;
-        }
-        > .btn_name {
+      margin-bottom: 2em;
+      > .left {
+        width: 36%;
+        font-family: 'CenturyStd-LightCondensed-VH', l serif;
+        padding-left: 2em;
+        > h4 {
           font-size: 24px;
+          margin-bottom: 30px;
+          letter-spacing: 0.07;
+        }
+        > .box_description {
+          font-family: 'CenturyStd-LightCondensed-VH', l serif;
+          > .box_address {
+            margin-bottom: 2em;
+            > img {
+              width: 10px;
+              color: #b3b3b3;
+              margin-right: 8px;
+            }
+            > span {
+              color: #b3b3b3;
+              text-align: left;
+              font-size: 16px;
+              line-height: 1.8;
+              letter-spacing: 0.04em;
+            }
+          }
+          > .box_time {
+            margin-bottom: 1em;
+            display: flex;
+            > .box_time_detail {
+              width: 90%;
+              display: flex;
+              flex-direction: column;
+              > span {
+                color: #b3b3b3;
+                text-align: left;
+                font-size: 16px;
+                line-height: 1.8;
+                letter-spacing: 0.04em;
+              }
+            }
+            > img {
+              width: 12px;
+              height: 12px;
+              color: #b3b3b3;
+              margin-right: 8px;
+              margin-top: 8px;
+            }
+          }
+          > .go_to_ggmap {
+            color: #9eaf3b;
+            font-size: 18px;
+            cursor: pointer;
+          }
+          > .go_to_ggmap:hover {
+            opacity: 0.7;
+          }
+          > .box_btn {
+            display: flex;
+            justify-content: space-between;
+            margin-top: 2em;
+            > .btn_see_detail {
+              width: 48%;
+              border: 1px white solid;
+              font-size: 18px;
+              color: white;
+              text-align: center;
+              padding: 8px;
+              cursor: pointer;
+            }
+            > .btn_see_detail:hover {
+              color: #9eaf3b;
+              border: #9eaf3b 1px solid;
+            }
+            > .btn_seservation {
+              width: 48%;
+              background: white;
+              font-size: 18px;
+              color: black;
+              text-align: center;
+              padding: 8px;
+              cursor: pointer;
+            }
+            > .btn_seservation:hover {
+              color: white;
+              background: #9eaf3b;
+            }
+          }
         }
       }
-      > .btn_reservation:hover {
-        opacity: 0.6;
+      > .right {
+        width: 56%;
+        padding: 0 20px;
+        line-height: 2.1;
+        position: relative;
+        > img {
+          width: 323px;
+          height: 323px;
+        }
+        > .iframe_map {
+          position: absolute;
+          top: -100px;
+          right: 30px;
+        }
       }
+    }
+    > .box_dear_custommer {
+      width: 100%;
+      max-width: 900px;
+      color: #fff;
+      font-size: 14px;
+      background: url('https://pizza4ps.com//img2017/location/bg_slashes.png')
+          repeat-y 0 0,
+        url('https://pizza4ps.com//img2017/location/bg_slashes.png') repeat-y
+          100% 0;
+      margin: 6em 0 8em 5%;
+      padding: 5px;
+      position: relative;
+      > div {
+        padding: 1em 1em;
+        font-family: 'Times New Roman';
+        font-style: normal;
+        letter-spacing: 0.07em;
+        line-height: 24px;
+      }
+    }
+    > .box_dear_custommer:before {
+      content: '';
+      position: absolute;
+      left: 0;
+      top: -5px;
+      width: 100%;
+      height: 5px;
+      background: url(//pizza4ps.com//img2017/location/bg_slashes.png) repeat-x
+        0 0;
+    }
+    > .box_dear_custommer:after {
+      content: '';
+      position: absolute;
+      left: 0;
+      bottom: -5px;
+      width: 100%;
+      height: 5px;
+      background: url('https://pizza4ps.com//img2017/location/bg_slashes.png')
+        repeat-x 0 100%;
     }
     > .left {
       width: 50%;
